0dc158c0c5e1eeaf416088f85596d772f5e07461
zev <zev@decent.xyz>
2024-01-05 14:25:59 -0400

    add base usdc

--- src/UsdcHelper.sol
+++ src/UsdcHelper.sol
@@ -76,6 +76,7 @@ contract UsdcHelper is TokenHelper, ChainAliases {
         _addTokenAddress(ethereum, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
         _addTokenAddress(arbitrum, 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8);
         _addTokenAddress(optimism, 0x7F5c764cBc14f9669B88837ca1490cCa17c31607);
+        _addTokenAddress(base, 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA);
         _addTokenAddress(polygon, 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
         _addTokenAddress(avalanche, 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E);
     }
@@ -84,6 +85,7 @@ contract UsdcHelper is TokenHelper, ChainAliases {
         _addWhaleAddress(ethereum, 0xcEe284F754E854890e311e3280b767F80797180d);
         _addWhaleAddress(arbitrum, 0xB38e8c17e38363aF6EbdCb3dAE12e0243582891D);
         _addWhaleAddress(optimism, 0xacD03D601e5bB1B275Bb94076fF46ED9D753435A);
+        _addWhaleAddress(base, 0x13A13869B814Be8F13B86e9875aB51bda882E391);
         _addWhaleAddress(polygon, 0xf89d7b9c864f589bbF53a82105107622B35EaA40);
         _addWhaleAddress(avalanche, 0x9f8c163cBA728e99993ABe7495F06c0A3c8Ac8b9);
     }
